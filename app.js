5f6ff4de93838d7c305d6fd833165874693e02ac
Goodness Maikoto <maikotogoodness@gmail.com>
2023-06-14 08:38:08 +0100

    made some corrections

---
+++ app.js
@@ -0,0 +1,11 @@
+// this is my js code
+// this is the start of my js design
+let number = document.getElementsById('number');
+let counter = 0;
+
+setInterval[() ==> {
+	if (counter == 65)
+	counter =+ 1;
+	number.html = '${counter}%';
+
+}]
